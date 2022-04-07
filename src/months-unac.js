515c659c741023e89c8265a7ef066122dca2eb9f
JoseBaldovino1014 <jdbaldovinor@unac.edu.co>
2022-04-06 20:52:20 -0500

    Cambios nuevos

--- src/months-unac.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.monthsUnac = void 0;
-function monthsUnac() {
-    return ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
-}
-exports.monthsUnac = monthsUnac;
-;
