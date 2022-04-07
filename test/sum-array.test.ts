0c28dba68e9adadcc3b58859906297a90e276049
JoseBaldovino1014 <jdbaldovinor@unac.edu.co>
2022-04-06 22:42:53 -0500

    Prueba Nueva

--- test/sum-array.test.ts
+++ test/sum-array.test.ts
@@ -3,3 +3,9 @@ import {sum} from '../src/sum-array';
 test("al resultado de 2 + 4 =6", () =>{
     expect(sum([2,4])).toBe(6);
     });
+    test("al resultado de 4 + 9 =13", () =>{
+        expect(sum([4,9])).toBe(13);
+        });
+        test("al resultado de 1 + 1 =2", () =>{
+            expect(sum([1,1])).toBe(2);
+            });
