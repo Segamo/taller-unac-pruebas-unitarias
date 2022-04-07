bf5d2d7e77a916849bd56fcc63871389df1aecbe
JoseBaldovino1014 <jdbaldovinor@unac.edu.co>
2022-04-06 21:10:30 -0500

    Guardar

---
+++ test/crazy-method.test.ts
@@ -0,0 +1,10 @@
+import { crazy } from "../src/crazy-method";
+
+test("Test crazy", () => {
+//probar
+expect(crazy(2,2)).toBe(2);
+});
+test("Test crazy", () => {
+    //probar
+    expect(crazy(2,1)).toBe(3);
+    });
