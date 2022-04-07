ec78fe931a3b8c884a0f5d715f3d3fded3894569
JoseBaldovino1014 <jdbaldovinor@unac.edu.co>
2022-04-06 22:19:22 -0500

    prueba5

--- test/months-unac.test.ts
+++ test/months-unac.test.ts
@@ -2,3 +2,6 @@ import { monthsUnac } from "../src/months-unac";
 test("Conts array contains Jan", () => {
     expect(monthsUnac()).toContain("Jan");
     });
+    test("Conts array contains Jan", () => {
+        expect(monthsUnac()).toContain("Feb");
+        });
