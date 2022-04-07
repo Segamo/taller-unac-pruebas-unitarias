7e3e472e79a61b00d1d4f9c13ad7edf243fa2fdd
JoseBaldovino1014 <jdbaldovinor@unac.edu.co>
2022-04-06 22:27:27 -0500

    Prueba Nueva

--- test/months-unac.test.ts
+++ test/months-unac.test.ts
@@ -2,6 +2,18 @@ import { monthsUnac } from "../src/months-unac";
 test("Conts array contains Jan", () => {
     expect(monthsUnac()).toContain("Jan");
     });
-    test("Conts array contains Jan", () => {
+    test("Conts array contains Feb", () => {
         expect(monthsUnac()).toContain("Feb");
         });
+        test("Conts array contains March", () => {
+            expect(monthsUnac()).toContain("March");
+            });
+            test("Conts array contains April", () => {
+                expect(monthsUnac()).toContain("April");
+                });
+                test("Conts array contains June", () => {
+                    expect(monthsUnac()).toContain("June");
+                    });
+                    test("Conts array contains May", () => {
+                        expect(monthsUnac()).toContain("May");
+                        });
