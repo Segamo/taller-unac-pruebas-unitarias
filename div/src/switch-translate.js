36f919bbb3fad0a8371569a1950930fd8d1b7e3f
francisco <fjsandovala@unac.edu.co>
2022-04-06 22:28:03 -0500

    segundo cambio

---
+++ div/src/switch-translate.js
@@ -0,0 +1,16 @@
+export function translateUnac(text) {
+    switch (text) {
+        case "hi": {
+            return "hola";
+        }
+        case "bye": {
+            return "adios";
+        }
+        case "moon": {
+            return "luna";
+        }
+        default: {
+            return "default";
+        }
+    }
+}
